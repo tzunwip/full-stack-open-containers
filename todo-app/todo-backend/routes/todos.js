e32fba7aa0e657162e0e732e79259ba1c522d4c7
Tzun Wong <tzun.wong@gmail.com>
2023-02-08 17:48:50 +0800

    12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -29,18 +29,23 @@ const findByIdMiddleware = async (req, res, next) => {
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+  await req.todo.delete()
   res.sendStatus(200);
 });
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  req.todo.text = req.body.text
+  req.todo.done = req.body.done
+  await req.todo.save((err) => {
+    if (err) return res.status(400).json(err)
+    res.sendStatus(204)
+  })
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
