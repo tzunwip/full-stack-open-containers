597e6e83226e94f006634e8e98f1bf8e9d8bfa51
Tzun Wong <tzun.wong@gmail.com>
2023-02-09 12:42:01 +0800

    fix react keys

--- todo-app/todo-frontend/src/Todos/List.js
+++ todo-app/todo-frontend/src/Todos/List.js
@@ -4,7 +4,7 @@ import Todo from './Todo.js'
 const TodoList = ({ todos, deleteTodo, completeTodo }) => {
   return (
     <>
-      {todos.map(todo => <Todo key={todo._id} todo={todo} deleteTodo={deleteTodo} completeTodo={completeTodo} />).reduce((acc, cur) => [...acc, <hr />, cur], [])}
+      {todos.map(todo => <Todo key={todo._id} todo={todo} deleteTodo={deleteTodo} completeTodo={completeTodo} />).reduce((acc, cur, index) => [...acc, <hr key={`hr-${index}`} />, cur], [])}
     </>
   )
 }
